5ff4c07123f0858d127b1be89cc4a2fff2af3a75
spencercz <48261199+spencercz@users.noreply.github.com>
2021-03-08 16:17:03 -0500

    Update gymbot.py

--- gymbot.py
+++ gymbot.py
@@ -8,7 +8,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import NoSuchElementException
-import re
 from time import sleep
 from getpass import getpass
 
