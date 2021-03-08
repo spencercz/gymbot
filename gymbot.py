ff2d1525bbdd6000dcfad1bc10fbca0216304aae
spencercz <48261199+spencercz@users.noreply.github.com>
2021-03-07 23:22:45 -0500

    made it work for real

--- gymbot.py
+++ gymbot.py
@@ -10,6 +10,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import NoSuchElementException
 import re
 from time import sleep
+from getpass import getpass
 
 _CSS_SELECTORS = {
     "reserve time" : "div.col-lg-3:nth-child(4) > a:nth-child(1) > span:nth-child(2)",
@@ -19,20 +20,32 @@ _CSS_SELECTORS = {
     "modal close"   : "#btnModalClose > span:nth-child(1)"
 }
 
+_TIMESLOTS = {
+    '1'  : '6:05 AM - 7:20 AM',
+    '2'  : '7:30 AM - 8:45 AM',
+    '3'  : '8:55 AM - 10:05 AM',
+    '4'  : '10:15 AM - 11:30 AM',
+    '5'  : '11:40 AM - 12:55 PM',
+    '6'  : '1:05 PM - 2:15 PM',
+    '7'  : '2:25 PM - 3:35 PM',
+    '8'  : '3:45 PM - 4:55 PM',
+    '9'  : '5:05 PM - 6:15 PM',
+    '10' : '6:25 PM - 7:35 PM',
+    '11' : '7:45 PM - 8:55 PM'
+}
+
 def setup():
     '''
     Opens the website, logs in the user, navigates to the workout time reservation, and chooses the lower floor
     '''
-    user = input("Enter your UDel username/email\n")
-    password = input("Enter your UDel password (don't worry, we won't keep this)\n")
+    user = input("UDel username/email: ")
+    password = getpass()
     driver.get("https://recreation.udel.edu/")
     driver.maximize_window()
     # Login
     driver.find_element_by_id("loginLink").click()
     sleep(2)
-    if check_exists_by_id(driver, "btnModalClose"):
-        driver.find_element_by_css_selector(_CSS_SELECTORS["modal close"]).click()
-    driver.find_element_by_xpath("//button[contains(.,'CAS')]").click()
+    driver.find_element_by_css_selector(".btn-linkedin").click()
     driver.find_element_by_id("udelnetid").send_keys(user)
     driver.find_element_by_id("pword").send_keys(password)
     driver.find_element_by_css_selector(_CSS_SELECTORS["login"]).click()
@@ -46,53 +59,8 @@ def signup(choice):
     while not button_exists:
         driver.refresh()
         time_map = make_map()
-        if choice == 1:
-            button_exists = time_map['6:05 AM - 7:20 AM'][0]
-        elif choice == 2:
-            button_exists = time_map['7:30 AM - 8:45 AM'][0]
-        elif choice == 3:
-            button_exists = time_map['8:55 AM - 10:05 AM'][0]
-        elif choice == 4:
-            button_exists = time_map['10:15 AM - 11:30 AM'][0]
-        elif choice == 5:
-            button_exists = time_map['11:40 AM - 12:55 PM'][0]
-        elif choice == 6:
-            button_exists = time_map['1:05 PM - 2:15 PM'][0]
-        elif choice == 7:
-            button_exists = time_map['2:25 PM - 3:35 PM'][0]
-        elif choice == 8:
-            button_exists = time_map['3:45 PM - 4:55 PM'][0]
-        elif choice == 9:
-            button_exists = time_map['5:05 PM - 6:15 PM'][0]
-        elif choice == 10:
-            button_exists = time_map['6:25 PM - 7:35 PM'][0]
-        elif choice == 11:
-            button_exists = time_map['7:45 PM - 8:55 PM'][0]
-
-    # yes. this code is bad. but i'm lazy
-    if choice == 1:
-        driver.find_element_by_css_selector(time_map['6:05 AM - 7:20 AM'][1]).click()
-    elif choice == 2:
-        driver.find_element_by_css_selector(time_map['7:30 AM - 8:45 AM'][1]).click()
-    elif choice == 3:
-        driver.find_element_by_css_selector(time_map['8:55 AM - 10:05 AM'][1]).click()
-    elif choice == 4:
-        driver.find_element_by_css_selector(time_map['10:15 AM - 11:30 AM'][1]).click()
-    elif choice == 5:
-        driver.find_element_by_css_selector(time_map['11:40 AM - 12:55 PM'][1]).click()
-    elif choice == 6:
-        driver.find_element_by_css_selector(time_map['1:05 PM - 2:15 PM'][1]).click()
-    elif choice == 7:
-        driver.find_element_by_css_selector(time_map['2:25 PM - 3:35 PM'][1]).click()
-    elif choice == 8:
-        driver.find_element_by_css_selector(time_map['3:45 PM - 4:55 PM'][1]).click()
-    elif choice == 9:
-        driver.find_element_by_css_selector(time_map['5:05 PM - 6:15 PM'][1]).click()
-    elif choice == 10:
-        driver.find_element_by_css_selector(time_map['6:25 PM - 7:35 PM'][1]).click()
-    elif choice == 11:
-        driver.find_element_by_css_selector(time_map['7:45 PM - 8:55 PM'][1]).click()
-    
+        button_exists = time_map[_TIMESLOTS[choice]][0]
+    driver.find_elements_by_css_selector(time_map[_TIMESLOTS[choice]][1]).click()
     driver.find_element_by_id("checkoutButton").click()
 
 def check_times(driver):
@@ -170,6 +138,9 @@ if __name__ == "__main__":
     else:
         print("Invalid response, exiting...")
         quit()
-    choice = int(input("Select your desired timeslot:\n1) 6:05 AM - 7:20 AM\n2) 7:30 AM - 8:45 AM\n3) 8:55 AM - 10:05 AM\n4) 10:15 AM - 11:30 AM\n5) 11:40 AM - 12:55 PM\n6)1:05 PM - 2:15 PM\n7)2:25 PM - 3:35 PM\n8) 3:45 PM - 4:55 PM\n9) 5:05 PM - 6:15 PM\n10) 6:25 PM - 7:35 PM\n11) 7:45 PM - 8:55 PM\n"))
+    choice = input("Select your desired timeslot:\n1) 6:05 AM - 7:20 AM\n2) 7:30 AM - 8:45 AM\n3) 8:55 AM - 10:05 AM\n4) 10:15 AM - 11:30 AM\n5) 11:40 AM - 12:55 PM\n6) 1:05 PM - 2:15 PM\n7) 2:25 PM - 3:35 PM\n8) 3:45 PM - 4:55 PM\n9) 5:05 PM - 6:15 PM\n10) 6:25 PM - 7:35 PM\n11) 7:45 PM - 8:55 PM\n")
+    if int(choice) > 11 or int(choice) < 1:
+        print("Invalid response, exiting...")
+        quit()
     setup()
     signup(choice)
